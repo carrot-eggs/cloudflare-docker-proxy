d8a5e5903ad5adc049f542caf51c4aa99b6d1cfa
carrot-eggs <35254441+carrot-eggs@users.noreply.github.com>
2024-06-28 09:49:15 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ const routes = {
   "ecr.carrotegg.icu": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.carrotegg.icu": dockerHub,
 };
 
 function routeByHosts(host) {
